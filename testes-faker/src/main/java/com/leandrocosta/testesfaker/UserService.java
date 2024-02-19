7cf490382603842009391bac09882d6422372aab
lcleandrocosta <lcleandrocosta@gmail.com>
2024-02-19 11:55:01 -0300

    Anotada a classe service

--- testes-faker/src/main/java/com/leandrocosta/testesfaker/UserService.java
+++ testes-faker/src/main/java/com/leandrocosta/testesfaker/UserService.java
@@ -3,6 +3,9 @@ package com.leandrocosta.testesfaker;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
     private Map<String, User> users = new HashMap<>();
     
